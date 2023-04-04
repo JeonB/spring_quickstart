23eeef62388d7ffa194ab906193827ac19262155
JeonB <newjeonb@gmail.com>
2023-04-04 18:57:24 +0900

    day02 class4 proceeding

--- src/main/java/day2/common/LogAdvice.java
+++ src/main/java/day2/common/LogAdvice.java
@@ -1,7 +1,8 @@
 package day2.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class LogAdvice {
-	public void printLog() {
-		System.out.println("[공통 로그] 비즈니스 수행 전 동작");
-	}
+	public void printLog(JoinPoint jp) {
+		System.out.println("[공통 로그] 비즈니스 수행 전 동작");	}
 }
