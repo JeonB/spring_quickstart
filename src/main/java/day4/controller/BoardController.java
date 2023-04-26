9f6d40325249f77ff8ffd3e59347c293a3a00d3b
JeonB <newjeonb@gmail.com>
2023-04-26 21:39:16 +0900

    class2 complete

--- src/main/java/day4/controller/BoardController.java
+++ src/main/java/day4/controller/BoardController.java
@@ -2,12 +2,26 @@ package day4.controller;
 
 import day4.model.board.BoardVO;
 import day4.model.board.impl.BoardDAO;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("board")
 public class BoardController {
+    //검색 조건 목록 설정, @RequestParam대신 사용
+    @ModelAttribute("conditonMap")
+    public Map<String,String> searchConditionMap(){
+        Map<String, String> conditionMap = new HashMap<>();
+        conditionMap.put("제목", "TITLE");
+        conditionMap.put("내용", "CONTENT");
+        return conditionMap;
+    }
 
     @RequestMapping("/")
     public String indexPage() {
@@ -22,8 +36,13 @@ public class BoardController {
     }
 
     @RequestMapping("/updateBoard.do")
-    public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
-        System.out.println("글 수정 처리");
+    public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
+        System.out.println("번호 : " + vo.getSeq());
+        System.out.println("제목 : " + vo.getTitle());
+        System.out.println("작성자 : " + vo.getWriter());
+        System.out.println("내용 : " + vo.getContent());
+        System.out.println("등록일 : " + vo.getRegDate());
+        System.out.println("조회수 : " + vo.getCnt());
         boardDAO.updateBoard(vo);
 
         return "redirect:getBoardList.do";
@@ -46,9 +65,11 @@ public class BoardController {
     }
 
     @RequestMapping("/getBoardList.do")
-    public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
-        System.out.println("글 목록 검색 처리");
+    public String getBoardList(@RequestParam(value = "searchCondition",defaultValue = "TITLE", required = false)String condtion,@RequestParam(value = "searchKeyword",defaultValue = "",required = false)String keyword, BoardDAO boardDAO, Model model,BoardVO vo) {
+//        System.out.println("글 목록 검색 처리");
+        System.out.println("검색 조건 : " + condtion);
+        System.out.println("검색 단어 : " + keyword);
         model.addAttribute("boardList", boardDAO.getBoardList(vo)); // Model 정보 저장
-        return "getBoardList"; // view 정보 저장
+        return "getBoardList"; // view 이름 리턴
     }
 }
--- web/WEB-INF/views/getBoardList.jsp
+++ web/WEB-INF/views/getBoardList.jsp
@@ -18,11 +18,14 @@
     <form action="getBoardList.do" method="post">
         <table border="1" cellpadding="0" cellspacing="0" width="700">
             <tr>
-                <td align="right"><select name="searchCondition">
-                    <option value="TITLE">제목
-                    <option value="CONTENT">내용
-                </select> <input name="searchKeyword" type="text"/> <input type="submit"
-                                                                           value="검색"/></td>
+                <td align="right">
+                    <select name="searchCondition">
+                        <c:forEach items="${conditonMap}" var="option">
+                            <option value="${option.value}">${option.key}</option>
+                        </c:forEach>
+                    </select>
+                    <input name="searchKeyword" type="text"/>
+                    <input type="submit" value="검색"/></td>
             </tr>
         </table>
     </form>
@@ -35,6 +38,7 @@
             <th bgcolor="orange" width="150">등록일</th>
             <th bgcolor="orange" width="100">조회수</th>
         </tr>
+
         <c:forEach items="${boardList}" var="board">
             <tr>
                 <td>${board.seq}</td>
